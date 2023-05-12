981c5ae86d85ae3728502bbbbf1f70fa6690f7a9
Gabrielius Batakis <gabrieliusbatakis@gmail.com>
2023-05-12 15:57:24 +0300

    added style, animations, more clean navigation menu

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,36 +1,92 @@
-import React from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { IoLibraryOutline } from "react-icons/io5";
-import { SlBookOpen } from "react-icons/sl";
+import { motion } from "framer-motion";
+import { useNavigate } from "react-router-dom";
+import { Canvas } from "@react-three/fiber";
+import { Suspense, useState } from "react";
 import styledHome from "./HomePage.module.css";
+import { Earth3D } from "../../components/Earth/Earth3D";
 
 const HomePage = () => {
+  // variables
+  const headlineText = {
+    library:
+      "Library page allows you to filter and sort countries based on your interests, so you can explore the world on your own terms.",
+    challenge:
+      "Challenge page puts your global knowledge to the test! It challenges you to identify flags, match capitals to countries, and approximate populations. Improve your understanding of the world's countries.",
+  };
+
+  // state
+  const [message, setMessage] = useState<string>("");
+
+  // motion
+  const itemVariants = {
+    initial: { opacity: 0, scale: 0 },
+    animate: { opacity: [0, 0, 1], scale: 1, transition: { delay: 2 } },
+  };
+  const headlineVariants = {
+    initial: { opacity: 1, scale: 1 },
+    animate: { opacity: [0, 0.5, 1], scale: 1 },
+  };
   // navigate
   const navigate = useNavigate();
+
   return (
-    <main className={styledHome.wrapper}>
-      <nav className={styledHome.nav}>
-        <ul className={styledHome.list}>
-          <li
-            className={`${styledHome.listItem} route`}
+    <main className={styledHome.wrapper} id="dark">
+      <Suspense fallback={null}>
+        <Canvas
+          style={{
+            height: "calc(100vh - 72px - 3em)",
+          }}
+        >
+          <Earth3D />
+        </Canvas>
+      </Suspense>
+
+      <div className={styledHome.heroEntry}>
+        <div className={styledHome.headlineContainer}>
+          <motion.h1
+            variants={itemVariants}
+            initial="initial"
+            animate="animate"
+          >
+            Explore the World Your Way
+          </motion.h1>
+          <div className={styledHome.messageContainer}>
+            {message ? (
+              <motion.h3
+                variants={headlineVariants}
+                initial="initial"
+                animate="animate"
+              >
+                {message}
+              </motion.h3>
+            ) : null}
+          </div>
+        </div>
+        <motion.ul className={styledHome.list}>
+          <motion.li
+            className={styledHome.listItem}
             onClick={() => navigate("library")}
+            variants={itemVariants}
+            initial="initial"
+            animate="animate"
+            onHoverStart={() => setMessage(headlineText.library)}
+            onHoverEnd={() => setMessage("")}
           >
             <p className={styledHome.link}>Library</p>
-            <IoLibraryOutline
-              className={`${styledHome.routeIcon} routeIconBackground`}
-            />
-          </li>
-          <li
-            className={`${styledHome.listItem} route`}
+          </motion.li>
+          <motion.li
+            className={styledHome.listItem}
             onClick={() => navigate("challenge")}
+            variants={itemVariants}
+            initial="initial"
+            animate="animate"
+            onHoverStart={() => setMessage(headlineText.challenge)}
+            onHoverEnd={() => setMessage("")}
           >
             <p className={styledHome.link}>Challenge</p>
-            <SlBookOpen
-              className={`${styledHome.routeIcon} routeIconBackground`}
-            />
-          </li>
-        </ul>
-      </nav>
+          </motion.li>
+        </motion.ul>
+      </div>
     </main>
   );
 };
