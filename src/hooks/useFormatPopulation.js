3b72b2b75fc6bc83d7847db0f325bea151098af4
Gabrielius Batakis <gabrieliusbatakis@gmail.com>
2023-05-12 14:49:11 +0300

    removed unused hook.

--- src/hooks/useFormatPopulation.js
+++
@@ -1,7 +0,0 @@
-const useFormatPopulation = (population) => {
-  if (population)
-    return population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-  return;
-};
-
-export default useFormatPopulation;
